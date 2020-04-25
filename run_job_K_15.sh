890f664cc659b929f37e6566482326b5edf57fc0
DDDOH <D__OH@outlook.com>
2020-04-24 22:02:54 -0700

    more job added

---
+++ run_job_K_15.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=test
+#
+# Account:
+#SBATCH --account=fc_nonsta
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Quality of Service:
+#SBATCH --qos=savio_normal
+#
+# Wall clock limit:
+#SBATCH --time=200:00:30
+
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yufeng_zheng@berkeley.edu
+#
+## Command(s) to run:
+# echo "hello world"
+# conda init bash
+# conda activate py3
+python -u python_script.py 15 100 25 25 > K_15_M_100_n_anchor_25_n_sensor_25_2nd_try.txt
